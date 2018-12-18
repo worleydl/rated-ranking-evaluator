4026ecad68c01c2255e077117eae8963297a129c
Matt Pearce <matt@flax.co.uk>
2018-12-18 15:17:34 +0000

    Display filter checkboxes on dashboard.

--- rre-server/src/main/resources/static/modules/main/config-service.js
+++ rre-server/src/main/resources/static/modules/main/config-service.js
@@ -17,11 +17,55 @@
          */
         var requestUrl = "/evaluation";
 
+        /**
+         * The metric list URL.
+         * @type {string}
+         */
+        var metricListUrl = requestUrl + "/metricList";
+
+        /**
+         * The version list URL.
+         * @type {string}
+         */
+        var versionListUrl = requestUrl + "/versionList";
+
+        /**
+         * The corpus list URL.
+         * @type {string}
+         */
+        var corpusListUrl = requestUrl + "/corpusList";
+
+        /**
+         * The topic list URL, requires corpus as a query param.
+         * @type {string}
+         */
+        var topicListUrl = requestUrl + "/topicList";
+
+        /**
+         * The query group list URL, requires topic and corpus as query params.
+         * @type {string}
+         */
+        var queryGroupListUrl = requestUrl + "/queryGroupList";
+
+        /**
+         * The filter evaluationURL.
+         * Takes corpus, topic, query group, versions and metrics as query
+         * params, all optional.
+         * @type {string}
+         */
+        var filterUrl = requestUrl + "/filter";
+
         init();
 
         return {
             requestInterval: requestInterval,
-            requestUrl: requestUrl
+            requestUrl: requestUrl,
+            metricListUrl: metricListUrl,
+            versionListUrl: versionListUrl,
+            corpusListUrl: corpusListUrl,
+            topicListUrl: topicListUrl,
+            queryGroupListUrl: queryGroupListUrl,
+            filterUrl: filterUrl
         };
 
         ////////////
--- rre-server/src/main/resources/static/modules/main/dashboard-controller.js
+++ rre-server/src/main/resources/static/modules/main/dashboard-controller.js
@@ -9,6 +9,12 @@
         // Scope vars
         vm.data = null;
         vm.isDetailOpen = [];
+        // Filtering lists
+        vm.metricList = [];
+        vm.versionList = [];
+        vm.corpusList = [];
+        vm.topicList = [];
+        vm.queryGroupList = [];
 
         // Methods
         vm.getMetricsCount = getMetricsCount;
@@ -23,16 +29,17 @@
         function activate() {
             DataService.getData().then(
                 function (response) {
-                    vm.data = response.data;
+                    vm.data = response;
+                    updateFilterLists();
                 },
                 function (error) {
-                    $log.error("DataService - Error while performing request:", response);
+                    $log.error("DataService - Error while performing request:", error);
                 }
             );
             $interval(function () {
                 DataService.getData().then(
                     function (response) {
-                        vm.data = response.data;
+                        vm.data = response;
                     },
                     function (error) {
                         $log.error("DataService - Error while performing request:", error);
@@ -51,6 +58,156 @@
             return 0;
         }
 
+        updateFilterLists = function() {
+            updateMetricList();
+            updateVersionList();
+            updateCorporaList();
+        }
 
+        updateMetricList = function() {
+            DataService.getMetricList().then(
+                function(data) {
+                    for (var i = 0; i < data.length; i ++) {
+                        if (!findByName(vm.metricList, data[i])) {
+                            vm.metricList.push({ name: data[i], selected: true });
+                        }
+                    }
+                },
+                function(error) {
+                    $log.error("DataService - error while fetching metrics: ", error);
+                }
+            );
+        }
+
+        $scope.toggleMetric = function(metric) {
+            var idx = vm.currentMetrics.indexOf(metric);
+            if (idx == -1) {
+                vm.currentMetrics.push(metric);
+            } else {
+                vm.currentMetrics.splice(idx, 1);
+            }
+        }
+
+        updateVersionList = function() {
+            DataService.getVersionList().then(
+                function(data) {
+                    for (var i = 0; i < data.length; i ++) {
+                        if (!findByName(vm.versionList, data[i])) {
+                            vm.versionList.push({ name: data[i], selected: true });
+                        }
+                    }
+                },
+                function(error) {
+                    $log.error("DataService - error while fetching versions: ", error);
+                }
+            );
+        }
+
+        findByName = function(objectArray, name) {
+            var ret = null;
+            for (i = 0; objectArray && i < objectArray.length; i ++) {
+                if (objectArray[i].name == name) {
+                    ret = objectArray[i];
+                    break;
+                }
+            }
+            return ret;
+        }
+
+        updateCorporaList = function() {
+            DataService.getCorpusList().then(
+                function(data) {
+                    for (var i = 0; i < data.length; i ++) {
+                        if (!findByName(vm.corpusList, data[i])) {
+                            vm.corpusList.push({ name: data[i], selected: true });
+                        }
+                    }
+                },
+                function(error) {
+                    $log.error("DataService - error while fetching corpora: ", error);
+                }
+            ).then(function() {
+                updateTopicList();
+            });
+        }
+
+        updateTopicList = function() {
+            for (var i = 0; i < vm.corpusList.length; i ++) {
+                if (vm.corpusList[i].selected) {
+                    var corpus = vm.corpusList[i];
+                    DataService.getTopicList(corpus.name).then(
+                        function(data) {
+                            for (var j = 0; j < data.length; j ++) {
+                                if (!findByNameAndCorpus(vm.topicList, data[j], corpus.name)) {
+                                    vm.topicList.push({
+                                        name: data[j],
+                                        corpus: corpus.name,
+                                        selected: true,
+                                        id: corpus.name + "_" + data[j]
+                                    });
+                                }
+                            }
+                        }.bind(this),
+                        function(error) {
+                            $log.error("DataService - error while fetching topics: ", error);
+                        }
+                    ).then(function() {
+                        updateQueryGroupList();
+                    });
+                }
+            }
+        }
+
+        findByNameAndCorpus = function(objectArray, name, corpus) {
+            var ret = null;
+            for (i = 0; objectArray && i < objectArray.length; i ++) {
+                if (objectArray[i].name == name && objectArray[i].corpus && objectArray[i].corpus == corpus) {
+                    ret = objectArray[i];
+                    break;
+                }
+            }
+            return ret;
+        }
+
+        updateQueryGroupList = function() {
+            for (var i = 0; i < vm.corpusList.length; i ++) {
+                for (var j = 0; j < vm.topicList.length; j ++) {
+                    var corpus = vm.corpusList[i];
+                    var topic = vm.topicList[j];
+                    if (corpus.selected && topic.corpus == corpus.name && topic.selected) {
+                        DataService.getQueryGroupList(corpus.name, topic.name).then(
+                            function(data) {
+                                for (i = 0; i < data.length; i ++) {
+                                    if (!findQueryGroupInList(data[i], corpus.name, topic.name)) {
+                                        vm.queryGroupList.push({
+                                            name: data[i],
+                                            selected: true,
+                                            corpus: corpus.name,
+                                            topic: topic.name,
+                                            id: corpus.name + "_" + topic.name + "_" + data[i]
+                                        });
+                                    }
+                                }
+                            }.bind(this),
+                            function(error) {
+                            }
+                        );
+                    }
+                }
+            }
+        }
+
+        findQueryGroupInList = function(name, corpus, topic) {
+            var ret = null;
+            for (i = 0; vm.queryGroupList && i < vm.queryGroupList.length; i ++) {
+                if (vm.queryGroupList[i].name == name
+                    && vm.queryGroupList[i].corpus && vm.queryGroupList[i].corpus == corpus
+                    && vm.queryGroupList[i].topic && vm.queryGroupList[i].topic == topic) {
+                    ret = vm.queryGroupList[i];
+                    break;
+                }
+            }
+            return ret;
+        }
     }
 })();
